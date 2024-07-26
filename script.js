2fede2b2cca2315fc07e394c29304ab27eb334ab
capt4inb <trieuphuisme@gmail.com>
2024-07-26 15:46:17 +0700

    add range length pass

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ title.textContent = 'Create random password with symbols';
 const inputPass = document.createElement('input');
 app.appendChild(inputPass);
 inputPass.placeholder = 'Enter your password';
+const valueLength = document.createElement('label');
+app.appendChild(valueLength);
+valueLength.textContent = `Password length: 8 `;
 const inputLength = document.createElement('input');
 app.appendChild(inputLength);
-inputLength.placeholder = 'Length of your password';
+inputLength.type = 'range';
+inputLength.value = '8';
+inputLength.min="8";
+inputLength.max="30";
+inputLength.classList.add('pass-length');
 // render frame-checkbox INCLUDE SYMBOLS
 const includeSymbols = document.createElement('div');
 app.appendChild(includeSymbols);
@@ -46,3 +53,35 @@ const button = document.createElement('button');
 app.appendChild(button);
 button.classList.add('btn');
 button.textContent = 'GENERATION';
+
+// FEATURE
+// const specialSymbols = ['!', '@', '#', '$', '%', '&', '*', '?', '+', '-', '_', '=', '(', ')', '{', '}', '[', ']', '|', '\\', '/', ':', ';', '<', '>', ',', '.', '`', '~'];
+// let randomSymbols = specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
+// const numbers = ['0', '1', '2', '3', '4','5','6', '7', '8', '9', '0'];
+// let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+// const maxLength = inputLength.maxLength ;
+// button.addEventListener('click',(e) => {
+//     if(inputPass.value === ''){
+//         alert('Please enter your password');
+//     }
+//     else{
+
+//     }
+// });
+// function generatePass(symbol,uppercaseLetters,lowercaseLetters,numbers) {
+// const symbol = '!@#$%^&*()<>()?/';
+// const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const lowercaseLetter = 'abcdefghijklmnopqrstuvwxyz';
+// const numbers = '0123456789';
+// }
+
+// const passwordLenge = 12;
+// const valuesymbols = true;
+// const valueuppercaseLetters = true;
+// const valuelowercaseLetters = true;
+// const valuenumbers = true;
+
+const updateSlider = () => {
+    document.querySelector('label').innerHTML = "Password length: " + inputLength.value;
+}
+inputLength.addEventListener('input', updateSlider);
