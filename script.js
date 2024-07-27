f8e549c7bca2426903d7c7c516c865cbd9ec64ec
capt4inb <trieuphuisme@gmail.com>
2024-07-27 16:10:09 +0700

    update generate pass

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ app.classList.add('app');
 const title = document.createElement('p');
 app.appendChild(title);
 title.textContent = 'Create random password with symbols';
+// input frame
+const inputFrame = document.createElement('div');
+app.appendChild(inputFrame);
+inputFrame.classList.add('inputFrame');
 const inputPass = document.createElement('input');
-app.appendChild(inputPass);
+inputPass.style.position = 'relative';
+const iconCopy = document.createElement('div');
+inputFrame.appendChild(iconCopy);
+iconCopy.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"/><path d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"/></g></svg>';
+inputFrame.appendChild(inputPass);
 inputPass.placeholder = 'Enter your password';
+// end input frame
 const valueLength = document.createElement('label');
 app.appendChild(valueLength);
 valueLength.textContent = `Password length: 8 `;
@@ -24,6 +33,7 @@ app.appendChild(includeSymbols);
 includeSymbols.classList.add('frame-checkbox');
 const symbolcheckbox = document.createElement('input');
 symbolcheckbox.type = 'checkbox';
+symbolcheckbox.id = 'symbols';
 includeSymbols.appendChild(symbolcheckbox);
 const paraSymbols = document.createElement('p');
 paraSymbols.textContent = 'Include symbols (?!@$%&*...)';
@@ -34,6 +44,7 @@ app.appendChild(includeUpercase);
 includeUpercase.classList.add('frame-checkbox');
 const upercasecheckbox = document.createElement('input');
 upercasecheckbox.type = 'checkbox';
+upercasecheckbox.id = 'uppercase';
 includeUpercase.appendChild(upercasecheckbox);
 const paraUpercase = document.createElement('p');
 paraUpercase.textContent = 'Include Upercase';
@@ -44,6 +55,7 @@ app.appendChild(includeNumber);
 includeNumber.classList.add('frame-checkbox');
 const numbercheckbox = document.createElement('input');
 numbercheckbox.type = 'checkbox';
+numbercheckbox.id = 'numbers';
 includeNumber.appendChild(numbercheckbox);
 const paraNumber = document.createElement('p');
 paraNumber.textContent = 'Include Number';
@@ -53,35 +65,47 @@ const button = document.createElement('button');
 app.appendChild(button);
 button.classList.add('btn');
 button.textContent = 'GENERATION';
-
 // FEATURE
-// const specialSymbols = ['!', '@', '#', '$', '%', '&', '*', '?', '+', '-', '_', '=', '(', ')', '{', '}', '[', ']', '|', '\\', '/', ':', ';', '<', '>', ',', '.', '`', '~'];
-// let randomSymbols = specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
-// const numbers = ['0', '1', '2', '3', '4','5','6', '7', '8', '9', '0'];
-// let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-// const maxLength = inputLength.maxLength ;
-// button.addEventListener('click',(e) => {
-//     if(inputPass.value === ''){
-//         alert('Please enter your password');
-//     }
-//     else{
-
-//     }
-// });
-// function generatePass(symbol,uppercaseLetters,lowercaseLetters,numbers) {
-// const symbol = '!@#$%^&*()<>()?/';
-// const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// const lowercaseLetter = 'abcdefghijklmnopqrstuvwxyz';
-// const numbers = '0123456789';
-// }
-
-// const passwordLenge = 12;
-// const valuesymbols = true;
-// const valueuppercaseLetters = true;
-// const valuelowercaseLetters = true;
-// const valuenumbers = true;
-
+// LENGTH PASSWORD
 const updateSlider = () => {
     document.querySelector('label').innerHTML = "Password length: " + inputLength.value;
 }
 inputLength.addEventListener('input', updateSlider);
+
+// GENERATE PASS 
+const characters = {
+    symbols : '!@#$%^&*()<>()?/',
+    uppercase : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    lowercaseLetter : 'abcdefghijklmnopqrstuvwxyz',
+    numbers : '0123456789'
+}
+const generateButon = document.querySelector('.btn');
+options = document.querySelectorAll('.frame-checkbox input');
+const generatePass = () => {
+    let defaultPass = "";
+    let randomPassword = "";
+    let passLength = inputLength.value;
+    options.forEach(option => {
+        if(option.checked)
+        {          
+            defaultPass += characters[option.id];
+        }
+        else{
+            defaultPass += characters[2];
+        }
+
+    });
+    for (let i = 0; i < passLength; i++){
+        randomPassword += defaultPass[Math.floor(Math.random() * defaultPass.length)];
+    }
+    inputPass.value = randomPassword;
+};
+generateButon.addEventListener("click" , generatePass);
+
+// COPY TO CLIPBOARD
+const copyToClipboard = () => {
+    navigator.clipboard.writeText(inputPass.value);
+    // inputPass.value = 'COPIED';
+    alert('Password copied to clipboard : ' + inputPass.value);
+}
+iconCopy.addEventListener('click', copyToClipboard);
